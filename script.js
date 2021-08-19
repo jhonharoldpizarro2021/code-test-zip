cf2ea17cae32199157a7086ec787f919aed91371
Jhon Pizarro Full Stack Developer <jhonharoldpizarro@gmail.com>
2021-08-18 20:35:38 -0500

    Prueba Finalizada

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/**
+ * Get data from api 
+ * @param {string} country 
+ * @param {string} code 
+ * @returns res
+ */
 async function getData(country,code) {
     let url = `http://api.zippopotam.us/${country}/${code}`;
     try {
@@ -8,33 +14,28 @@ async function getData(country,code) {
     }
 }
 
+/**
+ * Render data from fetch
+ * @param {string} country 
+ * @param {string} code 
+ */
 async function renderData(country,code) {
+    // send parameters for fetch data
     let data = await getData(country,code);
     if(data) {
-        console.log(data);
-        console.log(data.places[0]['place name']);
-        document.querySelector('#state').value = data.places[0]['state']; //data.places[0]['state'];
-        document.querySelector('#city').value = data.places[0]['place name']; //data.places[0]['place name'];
-
-        let results = document.querySelector('#results');
-        results.style.display = "block";
-
-        /* let html = '';
-        let htmlSegment = `<div class="user">
-                            <h2>${data.country} ${data.places[0].state}</h2>
-                            <div class="email"><a href="email:${data}">${data}</a></div>
-                        </div>`;
-
-        html += htmlSegment; */
-
-        //let container = document.querySelector('.container');
-        //container.innerHTML = html;
+        document.querySelector('#state').value = data.places[0]['state'];
+        document.querySelector('#city').value = data.places[0]['place name'];
+        document.querySelector('#results').style.display = "block";
+        document.querySelector('#get').style.display = "none";
+        document.querySelector('#clear').style.display = "block";
 	};
 }
 
+/**
+ * 
+ */
 function clickBtn() {
-    let btn = document.querySelector('#get');
-    btn.addEventListener("click", function( event ) {
+    document.querySelector('#get').addEventListener("click", function( event ) {
         let country = document.querySelector('#counrty').value.toLowerCase();
         let code =  document.querySelector('#zip').value;
         if(country != '' && code!=''){
@@ -42,4 +43,23 @@ function clickBtn() {
         }
     });
 }
-clickBtn();
+
+/**
+ * 
+ */
+function clear() {
+    document.querySelector('#clear').addEventListener("click", function( event ) {
+        document.querySelector('#state').value = '';
+        document.querySelector('#city').value = '';
+        document.querySelector('#zip').value = '';
+        document.querySelector('#counrty').value = '';
+        document.querySelector('#results').style.display = "none";
+        document.querySelector('#get').style.display = "block";
+        document.querySelector('#clear').style.display = "none";
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    clickBtn();
+    clear();
+});
